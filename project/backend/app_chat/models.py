35681c8a8e9b1fe111511e1a8edffee0ba6747db
developer <boris-plus@mail.ru>
2018-12-03 03:28:41 +0300

    длины полей снизил

--- project/backend/app_chat/models.py
+++ project/backend/app_chat/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Message(models.Model):
-    name = models.CharField(max_length=50)
-    message = models.CharField(max_length=128)
+    name = models.CharField(max_length=15)
+    message = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
